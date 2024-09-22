faed096b931f7fb0b76014031db37117a9c8e66f
ianwu0915 <wuhsinyen0915@gmail.com>
2024-09-22 18:00:46 -0500

    implement About part

---
+++ src/components/ToolboxItems.tsx
@@ -0,0 +1,34 @@
+import { TechIcon } from "./TechIcon";
+import { twMerge } from "tailwind-merge";
+export const ToolboxItems = ({
+  items,
+  className,
+  itemsWrapperClassName,
+
+}: {
+  items: { title: string; iconType: React.ElementType }[];
+  className?: string;
+    itemsWrapperClassName?: string;
+}) => {
+  const maskStyle = {
+    WebkitMaskImage:
+      "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+    maskImage:
+      "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+  };
+  return (
+    <div className={twMerge("flex", className)} style={maskStyle}>
+      <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWrapperClassName)}>
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <TechIcon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
